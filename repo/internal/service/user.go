1bc67313c7cffa8c9a913f26cddc4cf91b677f22
edaywalid <becharwalid9@gmail.com>
2025-06-30 01:37:20 +0100

    Implement UserService for user management

---
+++ repo/internal/service/user.go
@@ -0,0 +1,31 @@
+// internal/service/user.go
+package service
+
+import (
+	"context"
+
+	"github.com/lumbrjx/codek7/repo/internal/model"
+	"github.com/lumbrjx/codek7/repo/internal/repository"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, username string) (*model.User, error)
+	GetUser(ctx context.Context, id string) (*model.User, error)
+}
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) UserService {
+	return &userService{repo: repo}
+}
+
+func (s *userService) CreateUser(ctx context.Context, username string) (*model.User, error) {
+	user, err := s.repo.CreateUser(ctx, username)
+	return user, err
+}
+
+func (s *userService) GetUser(ctx context.Context, id string) (*model.User, error) {
+	return s.repo.GetUser(ctx, id)
+}
