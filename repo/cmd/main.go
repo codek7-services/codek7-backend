b74ccf9a4db064357e7418a13ba5bc4b335a84f8
edaywalid <becharwalid9@gmail.com>
2025-06-30 01:48:26 +0100

    Refactor MinIO and PostgreSQL configuration to use environment variables

--- repo/cmd/main.go
+++ repo/cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/lumbrjx/codek7/repo/internal/handler"
 	"github.com/lumbrjx/codek7/repo/internal/repository"
 	"github.com/lumbrjx/codek7/repo/internal/service"
@@ -14,15 +16,41 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-func main() {
-	// === Config ===
-	minioEndpoint := "localhost:9000"
-	minioAccessKey := "minioadmin"
-	minioSecretKey := "minioadmin"
-	minioBucket := "videos"
-	minioUseSSL := false
+var (
+	minioEndpoint,
+	minioAccessKey,
+	minioSecretKey,
+	minioBucket string
+	minioUseSSL bool
 
-	postgresDSN := "postgres://user:password@localhost:5432/codek7?sslmode=disable"
+	postgresDSN string
+)
+
+func init() {
+	_ = godotenv.Load(".env")
+
+	minioEndpoint = os.Getenv("MINIO_ENDPOINT")
+	minioAccessKey = os.Getenv("MINIO_ACCESS_KEY")
+	minioSecretKey = os.Getenv("MINIO_SECRET_KEY")
+	minioBucket = os.Getenv("MINIO_BUCKET")
+	minioUseSSL = os.Getenv("MINIO_USE_SSL") == "true"
+
+	if minioEndpoint == "" || minioAccessKey == "" || minioSecretKey == "" || minioBucket == "" {
+		log.Fatal("MinIO environment variables are not set")
+	}
+
+	postgresDSN = os.Getenv("POSTGRES_DSN")
+	if postgresDSN == "" {
+		log.Fatal("POSTGRES_DSN environment variable is not set")
+	}
+
+	if minioUseSSL {
+		log.Println("Using MinIO with SSL enabled")
+	}
+
+}
+
+func main() {
 
 	// === MinIO ===
 	minioClient, err := storage.New(minioEndpoint, minioAccessKey, minioSecretKey, minioBucket, minioUseSSL)
